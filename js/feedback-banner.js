75dcef88f2d87744463b0c19bc7d83703308188a
Borges-Porteous <miguel.borges-porteous@tbs-sct.gc.ca>
2023-11-17 12:17:26 -0500

    Auto stash before merge of "main" and "origin/main"

--- js/feedback-banner.js
+++ js/feedback-banner.js
@@ -9,10 +9,15 @@ alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha
 $('#wb-srch-sub').attr("disabled", true);
 
 //Seperate script to null all canada.ca links to a 404 page (including GC menu) and redirect some in menu
-/*$( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
+$( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
 	$("a[href='https://www.canada.ca/en/services/taxes.html']").attr("href", "https://cra-proto.github.io/cap/taxes.html").addClass("prototype");
 	$("a[href='https://www.canada.ca/en/services/taxes/income-tax.html']").attr("href", "https://cra-proto.github.io/cap/income-tax.html").addClass("prototype");
-    $("a[href='./cra.html']").attr("href", "https://cra-proto.github.io/cap/cra.html").addClass("prototype");
+	$("a[href='https://www.canada.ca/en/revenue-agency/corporate/contact-information.html']").attr("href", "https://cra-proto.github.io/cap/contact-cra.html").addClass("prototype");
+	$("a[href='https://www.canada.ca/en/revenue-agency/services/e-services/cra-login-services.html']").attr("href", "https://cra-proto.github.io/cap/cra-sign-in.html").addClass("prototype");
+	$("a[href='https://www.canada.ca/en/revenue-agency/services/e-services/my-business-account.html']").attr("href", "https://cra-proto.github.io/cap/my-business-account.html").addClass("prototype");
+	$("a[href='https://www.canada.ca/en/services/taxes/child-and-family-benefits.html']").attr("href", "https://cra-proto.github.io/cap/tax-credits-benefits.html").addClass("prototype");
+    $("a[href='https://www.canada.ca/en/services/taxes/income-tax/business-or-professional-income.html']").attr("href", "https://cra-proto.github.io/cap/business-or-professional-income.html").addClass("prototype");
+	$("a[href='./cra.html']").attr("href", "https://cra-proto.github.io/cap/cra.html").addClass("prototype");
 	$(".gcweb-menu a:not(.prototype)").attr("href", "https://cra-proto.github.io/cap/404.html");	
 	$("a[href*='canada.ca']").attr("href", "https://cra-proto.github.io/cap/404.html");
 	$("a[href*='.gc.ca/']").attr("href", "https://cra-proto.github.io/cap/404.html");
@@ -23,4 +28,4 @@ $(document).ready(function(){$("a[href*='canada.ca']").attr("href", "https://cra
 
 //Remove visited link design from 404 pages
 var visited_link_styling = "<style> a[href='https://cra-proto.github.io/cap/404.html']:visited{ color:#284162; } </style>"; $('head').append( visited_link_styling );
-*/
+
